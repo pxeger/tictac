516ced678be109a5c227190ce8dc282f76f2a1d2
Patrick Reader <_@pxeger.com>
2021-11-21 07:42:43 +0000

    clean up CLI

--- tictac/__main__.py
+++ tictac/__main__.py
@@ -1,11 +1,2 @@
-from sys import argv
-
-from tictac.parser import parse
-from tictac.interpreter import Interpreter
-
-code = argv[1]
-link = parse(code)
-interpreter = Interpreter(link)
-inputs = (eval(i) for i in argv[2:])
-out = interpreter.run(*inputs)
-print(*out, sep="\n")
+import tictac.cli
+tictac.cli.main()
