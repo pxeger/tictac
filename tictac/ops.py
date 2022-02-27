59e1313f30e82c4ca5f58e7f7f24eed535b13207
Patrick Reader <_@pxeger.com>
2022-02-27 13:27:05 +0000

    add basic arithmetic operations

--- tictac/ops.py
+++ tictac/ops.py
@@ -1,5 +1,7 @@
 from typing import Any, Callable
 
+from libgolf.vectorise import vectorise
+
 from tictac.interpreter import State
 
 
@@ -8,7 +10,22 @@ Op = tuple[int, OpFunction]
 
 OPS: dict[str, Op]
 OPS = {
-    "+": (2, lambda state, a, b: a + b),
+    # basic arithmetic
+    "+": (2, vectorise(lambda state, a, b: a + b)),
+    "-": (2, vectorise(lambda state, a, b: a - b)),
+    "*": (2, vectorise(lambda state, a, b: a * b)),
+    "÷": (2, vectorise(lambda state, a, b: a / b)),
+    ":": (2, vectorise(lambda state, a, b: a // b)),
+    "%": (2, vectorise(lambda state, a, b: a % b)),
+    "^": (2, vectorise(lambda state, a, b: a ** b)),
+    # logic
+    "&": (2, lambda state, a, b: a and b),
+    "|": (2, lambda state, a, b: a or b),
+    "<": (2, lambda state, a, b: a < b),
+    ">": (2, lambda state, a, b: a > b),
+    "≈": (2, vectorise(lambda state, a, b: a == b)),
+    "=": (2, lambda state, a, b: a == b),
+    "¬": (1, lambda state, a: not a),
 }
 
 MODIFIERS: dict[str, Callable[[Callable[[], Op]], Op]]
